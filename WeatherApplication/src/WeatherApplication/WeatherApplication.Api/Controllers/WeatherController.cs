be64070c56f9bbd6f00d57b6ada68327aae0e772
Li Wirström <li.wirstrom@ffcg.se>
2018-02-14 17:21:13 +0100

    Added EF to API controller

--- WeatherApplication/src/WeatherApplication/WeatherApplication.Api/Controllers/WeatherController.cs
+++ WeatherApplication/src/WeatherApplication/WeatherApplication.Api/Controllers/WeatherController.cs
@@ -10,8 +10,6 @@ namespace WeatherApplication.Api.Controllers
 	[Route("api/[controller]")]
 	public class WeatherController : Controller
 	{
-		//private string ConnectionString = @"Server=(localdb)\MSSQLLocalDB;Initial Catalog=WeatherAPI;Integrated Security=SSPI;Trusted_Connection=yes;";
-
 		[HttpGet]
 		public IActionResult Get()
 		{
@@ -27,22 +25,6 @@ namespace WeatherApplication.Api.Controllers
 		{
 			try
 			{
-				//var Connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB;Initial Catalog=WeatherAPI;Integrated Security=SSPI;Trusted_Connection=yes;");
-				//Connection.Open();
-				//var command = Connection.CreateCommand();
-				//command.CommandText = "SELECT * FROM Stations WHERE Id = @IdParam ";
-
-				//var param = new SqlParameter();
-				//param.ParameterName = "@IdParam";
-				//param.Value = id;
-				//command.Parameters.Add(param);
-
-				//var reader = command.ExecuteReader();
-				//reader.Read();
-				//var dataReaderWrapper = new SqlDataReaderWrapper(reader);
-				//var weatherStation = StationViewModel.Create(dataReaderWrapper);
-
-				//return Ok(weatherStation);
 				using (var context = new WeatherContext())
 				{
 					var station = context.Stations.First(x => x.Id == id);
@@ -54,6 +36,5 @@ namespace WeatherApplication.Api.Controllers
 				return NotFound();
 			}
 		}
-
 	}
 }
