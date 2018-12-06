5ffb9a93572c9f796e28b0a82f17dfeb097ed94f
Li Wirström <li.wirstrom@ffcg.se>
2018-12-06 23:09:57 +0100

    started day3 part1

--- AdventofCode/AdventOfCode_2018/AdventOfCode_2018/AdventofCode_2018_Day3_Tests/UnitTest1.cs
+++ AdventofCode/AdventOfCode_2018/AdventOfCode_2018/AdventofCode_2018_Day3_Tests/UnitTest1.cs
@@ -1,13 +1,128 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventofCode_2018_Day3_Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-    }
+	    private ClaimCalculator _claimCalculator;
+
+	    [TestInitialize]
+	    public void Setup()
+	    {
+			_claimCalculator = new ClaimCalculator();
+	    }
+
+	    [TestMethod]
+	    public void Should_Draw()
+	    {
+		    var input = new List<string> { "#1 @ 1,3: 4x4" };
+
+		    var elfClaim = new ElfClaim(input[0]);
+
+			Assert.AreEqual(elfClaim.Id, 1);
+		    Assert.AreEqual(elfClaim.Coordinate.X, 1);
+		    Assert.AreEqual(elfClaim.Coordinate.Y, 3);
+			Assert.AreEqual(elfClaim.Height, 4);
+			Assert.AreEqual(elfClaim.Width, 4);
+		}
+
+		[TestMethod]
+	    public void Should_Calculate_Area()
+	    {
+		    var input = new List<string> { "#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2" };
+
+		    var elfClaims = new List<ElfClaim>();
+
+		    foreach (var claim in input)
+		    {
+				elfClaims.Add(new ElfClaim(claim));   
+		    }
+
+		    var overlap = _claimCalculator.GetOverlap(elfClaims);
+
+			Assert.AreEqual(4, overlap);
+	    }
+
+	    [TestMethod]
+	    public void Answer_Input_part1()
+	    {
+		    var input = File.ReadAllLines("../../Day3_input.txt");
+
+		    var elfClaims = new List<ElfClaim>();
+
+		    var expected = new int[,] { { 1, 3 }, { 2, 3 }, { 1, 4 }, { 2, 4 } };
+		    foreach (var claim in input)
+		    {
+			    elfClaims.Add(new ElfClaim(claim));
+		    }
+
+		    var overlap = _claimCalculator.GetOverlap(elfClaims);
+
+		    Assert.AreEqual(4, overlap);
+	    }
+
+	}
+
+	public class ClaimCalculator
+	{
+		public int GetOverlap(List<ElfClaim> elfClaims)
+		{
+			var claimedFabric = new Dictionary<string, int>();
+			var overlap = 0;
+			foreach (var elfClaim in elfClaims)
+			{
+				var startX = elfClaim.Coordinate.X;
+				for (int i = startX; i <= startX+elfClaim.Height; i++)
+				{
+					var startY = elfClaim.Coordinate.Y;
+					for (int j = startY; j <= startY+elfClaim.Width; j++)
+					{
+						var key = $"{i},{j}";
+						if (claimedFabric.ContainsKey(key))
+						{
+							claimedFabric[key] += 1;
+						}
+						else
+						{
+							claimedFabric[key] = 1;
+						}
+					}	
+				}
+			}
+
+			return claimedFabric.Count(c => c.Value > 1);
+			
+		}
+	}
+
+
+	public class ElfClaim
+	{
+		public ElfClaim(string claim)
+		{
+			var splittedClaim = claim.Split(' ');
+			Id = int.Parse(splittedClaim[0].Replace("#", ""));
+			Coordinate.X = int.Parse(splittedClaim[2].Split(',')[0]);
+			Coordinate.Y = int.Parse(splittedClaim[2].Split(',')[1].Replace(":", ""));
+			Height = int.Parse(splittedClaim[3].Split('x')[0]);
+			Width = int.Parse(splittedClaim[3].Split('x')[1]);
+
+		}
+
+		public int Id { get; private set; }
+		public int Height { get; private set; }
+		public int Width { get; private set; }
+		public Coordinate Coordinate { get; private set; } = new Coordinate();
+	}
+
+	public class Coordinate
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+	}
 }
